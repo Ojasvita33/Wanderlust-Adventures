65adb629ef607425baaf6a813402f1f6b70d8ba2
Ojasvita <157558684+Ojasvita33@users.noreply.github.com>
2025-05-06 15:22:59 +0000

    Middleware

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,4 +1,4 @@
 exports.errorMiddleware = (err, req, res, next) => {
   console.error('Error:', err.stack);
-  res.status(err.status || 500).render('error', { title: 'Error', message: 'Something went wrong!' }); // Render error page
+  res.status(err.status || 500).render('error', { title: 'Error', message: 'Something went wrong!' }); 
 };
