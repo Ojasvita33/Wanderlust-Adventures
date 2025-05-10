ecfa7e47607625b1d9f7bd2669ee45ce1649338a
Ojasvita <157558684+Ojasvita33@users.noreply.github.com>
2025-05-11 00:38:40 +0530

    Routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,25 +2,15 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const { body } = require('express-validator');
 
 router.get('/signup', authController.getSignup);
-router.post('/signup', [
-    body('name').trim().notEmpty().escape(),
-    body('username').trim().notEmpty().escape(),
-    body('email').isEmail().normalizeEmail(),
-    body('password').isLength({ min: 6 })
-], authController.postSignup);
+router.post('/signup', authController.postSignup);
 
 router.get('/login', authController.getLogin);
-router.post('/login', [
-    body('username').trim().notEmpty().escape(),
-    body('password').notEmpty()
-], authController.postLogin);
+router.post('/login', authController.postLogin);
 
 router.get('/logout', authController.getLogout);
 
-// New route to set the redirect URL in the session
 router.get('/set-redirect', authController.setRedirect);
 
 module.exports = router;
