e48d4b1cc177ea8c1e280622cc201785384c8176
Ojasvita <157558684+Ojasvita33@users.noreply.github.com>
2025-05-11 00:25:15 +0530

    Middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,3 +1,4 @@
+// middleware/authMiddleware.js
 const User = require('../models/User');
 
 exports.authMiddleware = async (req, res, next) => {
@@ -5,33 +6,13 @@ exports.authMiddleware = async (req, res, next) => {
         try {
             const user = await User.findById(req.session.userId);
             if (!user) {
-                return res.redirect('/auth/login'); // Redirect instead of JSON
+                return res.redirect('/auth/login');
             }
             req.user = user;
             next();
         } catch (err) {
             console.error('Error in authMiddleware:', err);
-            res.redirect('/auth/login'); // Redirect on error
-        }
-    } else {
-        res.redirect('/auth/login');
-    }
-};
-
-// const User = require('../models/User');
-
-exports.requireLogin = async (req, res, next) => {
-    if (req.session && req.session.userId) {
-        try {
-            const user = await User.findById(req.session.userId);
-            if (!user) {
-                return res.redirect('/auth/login'); // Redirect instead of JSON
-            }
-            req.user = user;
-            next();
-        } catch (err) {
-            console.error('Error in requireLogin:', err);
-            res.redirect('/auth/login'); // Redirect on error
+            res.redirect('/auth/login');
         }
     } else {
         res.redirect('/auth/login');
