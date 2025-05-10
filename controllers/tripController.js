73ff58c3976c3ea180c38aad2e619e691de9b051
Ojasvita <157558684+Ojasvita33@users.noreply.github.com>
2025-05-11 00:19:33 +0530

    Controller

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -14,23 +14,18 @@ exports.getAllTrips = async (req, res) => {
 // Fetch trip by ID
 exports.getTripDetails = async (req, res) => {
     const tripId = req.params.id;
-    console.log('Fetching trip details for ID:', tripId);
     try {
         const trip = await Trip.findById(tripId);
-        console.log('Trip found:', trip);
         if (!trip) {
-            console.log('Trip not found for ID:', tripId);
             return res.status(404).render('404', { title: 'Trip Not Found' });
         }
-        res.render('tripdetails', {
+        res.render('tripDetails', {
             title: trip.name,
             trip: trip,
             user: req.session.user,
             currentUrl: req.originalUrl
         });
-        console.log('Trip details rendered successfully');
     } catch (error) {
-        console.error('Error fetching trip details:', error);
         res.status(500).send('Error fetching trip details');
     }
 };
