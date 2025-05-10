c89b606fd49f7efc6456dd60db0d4c3683048df3
Ojasvita <157558684+Ojasvita33@users.noreply.github.com>
2025-05-11 01:00:13 +0530

    Routes

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const bookingController = require('../controllers/bookingController');
-const { requireLogin } = require('../middleware/authMiddleware');
+const { authMiddleware } = require('../middleware/authMiddleware'); // Using authMiddleware
 
 // Route to proceed to the payment/confirmation page
-router.post('/payment', requireLogin, bookingController.proceedToPayment);
-
+router.post('/payment', authMiddleware, bookingController.proceedToPayment); 
 // Route to process the booking
-router.post('/book', requireLogin, bookingController.processBooking);
-
+router.post('/book', authMiddleware, bookingController.processBooking); 
 // Route to view booking details
-router.get('/:id', requireLogin, bookingController.getBookingById);
-
+router.get('/:id', authMiddleware, bookingController.getBookingById); 
 // Route to cancel a booking
-router.delete('/:id', requireLogin, bookingController.deleteBooking);
-
+router.delete('/:id', authMiddleware, bookingController.deleteBooking); 
 module.exports = router;
