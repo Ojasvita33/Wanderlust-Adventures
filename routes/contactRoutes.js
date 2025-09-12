f57794525449fe0be9ca8d5700908ad594d4cbd6
Ojasvita <157558684+Ojasvita33@users.noreply.github.com>
2025-09-12 18:19:26 +0530

    Update contactRoutes.js

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router();
-const contactController = require('../controllers/contactController');
-
-router.get('/', contactController.getContactPage);
-router.post('/', contactController.handleContactFormSubmission);
-
-
-module.exports = router;
-=======
 const express = require('express');
 const router = express.Router();
 const contactController = require('../controllers/contactController');
@@ -17,4 +6,3 @@ router.get('/', contactController.getContactPage);
 router.post('/', contactController.handleContactFormSubmission);
 
 module.exports = router;
->>>>>>> 8fd1a0cd3fda0a9c3742b3970ecc4ba44cc3589f
