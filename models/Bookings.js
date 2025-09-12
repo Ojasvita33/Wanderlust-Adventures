b21af36966f6b1bcd4daf906506b152d1a54327a
Ojasvita <157558684+Ojasvita33@users.noreply.github.com>
2025-09-12 18:17:04 +0530

    Update Bookings.js

--- models/Bookings.js
+++ models/Bookings.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -24,30 +23,3 @@ const bookingSchema = new Schema({
 });
 
 module.exports = mongoose.model('Booking', bookingSchema);
-=======
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const bookingSchema = new Schema({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    trip: {
-        type: Schema.Types.ObjectId,
-        ref: 'Trip',
-        required: true
-    },
-    bookingDate: {
-        type: Date,
-        default: Date.now
-    },
-    tripStartDate: {
-        type: Date,
-        required: true
-    }
-});
-
-module.exports = mongoose.model('Booking', bookingSchema);
->>>>>>> 8fd1a0cd3fda0a9c3742b3970ecc4ba44cc3589f
