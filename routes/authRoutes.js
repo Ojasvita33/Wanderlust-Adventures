72f42757ca5eb518191298885d36f8e6c4b93c38
Ojasvita <157558684+Ojasvita33@users.noreply.github.com>
2025-09-12 18:18:45 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // authRoutes.js
 const express = require('express');
 const router = express.Router();
@@ -15,21 +14,3 @@ router.get('/logout', authController.getLogout);
 router.get('/set-redirect', authController.setRedirect);
 
 module.exports = router;
-=======
-// authRoutes.js
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.get('/signup', authController.getSignup);
-router.post('/signup', authController.postSignup);
-
-router.get('/login', authController.getLogin);
-router.post('/login', authController.postLogin);
-
-router.get('/logout', authController.getLogout);
-
-router.get('/set-redirect', authController.setRedirect);
-
-module.exports = router;
->>>>>>> 8fd1a0cd3fda0a9c3742b3970ecc4ba44cc3589f
