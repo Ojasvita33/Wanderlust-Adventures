68f9244db9f43f6f7ccf6f854e9bda147920da5a
Ojasvita33 <ojrn777@gmail.com>
2025-09-12 18:02:44 +0530

    Remove all merge conflict markers and resolve conflicts

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -4,11 +4,9 @@ const Trip = require('../models/Trip');
 // Fetch all trips
 exports.getAllTrips = async (req, res) => {
     try {
-        const trips = await Trip.find();
         res.render('trips', { title: 'All Our Adventures', trips, user: req.session.user }); // Render a 'trips.ejs' view
-    } catch (err) {
         console.error(err);
-        res.status(500).send('Error fetching trips');
+        const Trip = require('../models/Trip');
     }
 };
 
