19d1a9ca5fe6ed7630a8c2baba58dc2d264e019b
Ojasvita <157558684+Ojasvita33@users.noreply.github.com>
2025-09-12 18:17:27 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // models/User.js
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
@@ -25,31 +24,3 @@ const userSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('User', userSchema);
-=======
-// models/User.js
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-
-const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    username: {
-        type: String,
-        unique: true,
-        required: true,
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-});
-
-module.exports = mongoose.model('User', userSchema);
->>>>>>> 8fd1a0cd3fda0a9c3742b3970ecc4ba44cc3589f
